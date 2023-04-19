f8467891ebdd5da9fa28db1d757bcd9b1125373e
xxyouszz <125680172+xxyouszz@users.noreply.github.com>
2023-04-19 22:56:01 +0200

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -8,11 +8,11 @@
 * Return: nothing
 **/
 
-	void print_name(char *name, void (*f)(char *))
-	{
-		if (name == NULL || f == NULL)
-			return;
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
 
 
-		f(name);
-	}
+	f(name);
+}
